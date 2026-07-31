ed302d722c92bf640bc9f5a6afaa69b65bdd0609
floyd-2499 <floydmanuel011@gmail.com>
2026-07-31 23:49:32 +0530

    Add footer

--- src/pages/components/footer/index.js
+++ src/pages/components/footer/index.js
@@ -1,4 +1,8 @@
 import Link from "next/link"
+import Image from "next/image";
+
+import logo from "../../../assets/logo/logo-dark.svg";
+import { PiSmileyWink } from "react-icons/pi";
 import { menuData } from "../header"
 import { socialMediaContents } from "../hero"
 import styles from "./styles.module.scss"
@@ -7,7 +11,13 @@ const Footer = () => {
     return (
         <div className={styles["footer-main"]}>
             <div className={styles["footer-top"]}>
-                <div className={styles["footer-left"]}></div>
+                <div className={styles["footer-left"]}>
+                    <div className={styles["footer-left-main-text"]}>Thanks for visiting <PiSmileyWink className={styles["smiley"]} /></div>
+                    <Image className={styles["footer-left-logo"]} alt="Floyd Manuel Dsouza Portfolio Logo" src={logo} />
+                    <div className={styles["footer-left-sub-text"]}>FLOYD MANUEL DSOUZA</div>
+                    <div className={styles["footer-left-sub-text"]}>floydmanuel011@gmail.com</div>
+                    <div className={styles["footer-left-sub-text"]}>+91 9740 250911</div>
+                </div>
                 <div className={styles["footer-mid"]}>
                     {menuData?.map((menu) => {
                         return (
